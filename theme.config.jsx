8266518d79a7eebc2dfe195ea3bca51d664ebef6
Sheng Chou <chousheng@gmail.com>
2023-06-23 09:02:02 -0700

    Hello world

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+export default {
+  logo: <span>Ray Tracing from C++ to Rust</span>,
+  project: {
+    link: 'https://github.com/chousheng/raytracing-cpp-rust'
+  },
+  footer: {
+    component: null
+  }
+}
