bc53e3151a4f428353e9afff044940af3d449c20
Sheng Chou <chousheng@gmail.com>
2023-07-12 14:46:30 -0700

    Convert chapter 4 from markdeep to mdx (rays-and-camera-and-background.mdx)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx'
-})
- 
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+  latex: true,
+});
+
 // module.exports = withNextra({ /* other next.js config */ })
-module.exports = withNextra()
+module.exports = withNextra();
