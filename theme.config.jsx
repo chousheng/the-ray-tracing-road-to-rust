cd66567bbbf7c1416ed862162f155548faf14b2b
Sheng Chou <chousheng@gmail.com>
2023-07-17 13:55:15 -0700

    Import Tab, Tabs, and Callout components automatically

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,15 @@
+import { Tab, Tabs } from "components/Tabs";
+import { Callout } from "nextra/components";
+
 const config = {
   logo: <span>The Ray Tracing Road to Rust</span>,
   project: {
     link: "https://github.com/chousheng/raytracing-cpp-rust",
+  },
+  components: {
+    Callout: Callout,
+    Tabs: Tabs,
+    Tab: Tab,
   },
   footer: {
     component: null,
