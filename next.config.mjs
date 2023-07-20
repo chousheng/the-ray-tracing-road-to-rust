6d39b10daf97b4768176756f93ad5e0e5122dc68
Sheng Chou <chousheng@gmail.com>
2023-07-20 10:57:40 -0700

    Update format settings

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@ import nextra from "nextra";
 import smartypants from "remark-smartypants";
 
 const withNextra = nextra({
-    theme: "nextra-theme-docs",
-    themeConfig: "./theme.config.jsx",
-    latex: true,
-    mdxOptions: {
-        remarkPlugins: [smartypants],
-    },
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+  latex: true,
+  mdxOptions: {
+    remarkPlugins: [smartypants],
+  },
 });
 
 export default withNextra();
