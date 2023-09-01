d4ce0a3397488d69f2546377b5601de956d5869a
Sheng Chou <chousheng@gmail.com>
2023-09-01 15:54:21 -0700

    Add Open Graph

--- theme.config.jsx
+++ theme.config.jsx
@@ -9,11 +9,33 @@ const config = {
       titleTemplate: "%s – The Ray Tracing Road to Rust 🦀",
     };
   },
+  head: () => {
+    const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL
+      : "http://localhost:3000";
+
+    const appTitle = "The Ray Tracing Road to Rust";
+    const description = "A tutorial to learn Rust by writing a ray tracer.";
+    const ogImage = baseUrl + "/og.jpg";
+
+    return (
+      <>
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="apple-mobile-web-app-title" content={appTitle} />
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={ogImage} />
+      </>
+    );
+  },
   project: {
-    link: "https://github.com/chousheng/theraytracingroadtorust",
+    link: "https://github.com/chousheng/the-ray-tracing-road-to-rust",
   },
   docsRepositoryBase:
-    "https://github.com/chousheng/theraytracingroadtorust/tree/main",
+    "https://github.com/chousheng/the-ray-tracing-road-to-rust/tree/main",
   components: {
     Callout: Callout,
     Tabs: Tabs,
