1ab8038a074aa6c918202dd14f3506a7afd5771f
Sheng Chou <chousheng@gmail.com>
2023-09-02 08:30:56 -0700

    Refactor open graph settings

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,56 +1,45 @@
 import { Fig, Img } from "components/Image";
 import { Tab, Tabs } from "components/Tabs";
 import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
 import { Callout } from "nextra/components";
 
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL
   : "http://localhost:3000";
 
-const title = "The Ray Tracing Road to Rust";
-const titleWithEmoji = "The Ray Tracing Road to Rust 🦀";
+const websiteTitle = "The Ray Tracing Road to Rust";
+const websiteTitleWithEmoji = "The Ray Tracing Road to Rust 🦀";
 const description = "A tutorial to learn Rust by writing a ray tracer.";
 const ogImage = baseUrl + "/og.jpg";
 
 const config = {
-  logo: <span>{titleWithEmoji}</span>,
+  logo: <span>{websiteTitleWithEmoji}</span>,
   useNextSeoProps: () => {
     const { asPath } = useRouter();
-
+    const { title } = useConfig();
     return {
-      titleTemplate: asPath === "/" ? title : `%s - ${title}`,
+      title: asPath === "/" ? websiteTitle : title + " - " + websiteTitle,
       description: description,
       openGraph: {
-        type: "website",
-        title: titleWithEmoji,
+        title:
+          asPath === "/"
+            ? websiteTitleWithEmoji
+            : title + " - " + websiteTitleWithEmoji,
         images: [{ url: ogImage }],
       },
       twitter: {
         cardType: "summary_large_image",
       },
     };
-
-    if (asPath === "/") {
-      return {
-        titleTemplate: title,
-      };
-    } else {
-      return {
-        titleTemplate: `%s – ${title}`,
-      };
-    }
   },
   head: () => {
     return (
       <>
-        {/* <meta httpEquiv="Content-Language" content="en" />
-        <meta name="apple-mobile-web-app-title" content={title} />
-        <meta name="msapplication-TileColor" content="#fff" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="description" content={description} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={ogImage} /> */}
+        <meta name="apple-mobile-web-app-title" content={websiteTitle} />
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta httpEquiv="Content-Language" content="en" />
       </>
     );
   },
