4281517a08b094a1f8a7a91fb1d8f82246289f2f
Sheng Chou <chousheng@gmail.com>
2023-09-01 20:08:58 -0700

    Refactor open graph handling

--- theme.config.jsx
+++ theme.config.jsx
@@ -7,14 +7,29 @@ const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL
   : "http://localhost:3000";
 
-const title = "The Ray Tracing Road to Rust 🦀";
+const title = "The Ray Tracing Road to Rust";
+const titleWithEmoji = "The Ray Tracing Road to Rust 🦀";
 const description = "A tutorial to learn Rust by writing a ray tracer.";
 const ogImage = baseUrl + "/og.jpg";
 
 const config = {
-  logo: <span>{title}</span>,
+  logo: <span>{titleWithEmoji}</span>,
   useNextSeoProps: () => {
     const { asPath } = useRouter();
+
+    return {
+      titleTemplate: asPath === "/" ? title : `%s - ${title}`,
+      description: description,
+      openGraph: {
+        type: "website",
+        title: titleWithEmoji,
+        images: [{ url: ogImage }],
+      },
+      twitter: {
+        cardType: "summary_large_image",
+      },
+    };
+
     if (asPath === "/") {
       return {
         titleTemplate: title,
@@ -28,14 +43,14 @@ const config = {
   head: () => {
     return (
       <>
-        <meta httpEquiv="Content-Language" content="en" />
+        {/* <meta httpEquiv="Content-Language" content="en" />
         <meta name="apple-mobile-web-app-title" content={title} />
         <meta name="msapplication-TileColor" content="#fff" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="description" content={description} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={ogImage} />
+        <meta property="og:image" content={ogImage} /> */}
       </>
     );
   },
