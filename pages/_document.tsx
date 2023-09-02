531e4b246db0353b6d6327ded85410caf85fc875
Sheng Chou <chousheng@gmail.com>
2023-09-01 20:08:32 -0700

    Add production guard in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,18 +6,22 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-84TDV5T5TR"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
+        {process.env.NEXT_PUBLIC_VERCEL_ENV == "production" && (
+          <>
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-84TDV5T5TR"
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-84TDV5T5TR');`,
-          }}
-        />
+              }}
+            />
+          </>
+        )}
       </Head>
       <body>
         <Main />
